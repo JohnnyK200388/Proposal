1c59d914ac3a4512d38351edd5262d4d88ff00b7
JohnnyK200388 <129217850+JohnnyK200388@users.noreply.github.com>
2023-04-04 23:41:44 -0400

    Add files via upload

---
+++ preposalog.py
@@ -0,0 +1,14 @@
+ # Preproposal
+
+## What idea(s) do you have for your final project?
+
+TODO // some plans that I have is making something like a twitter bot that takes in a text source and based off the text source picks out a random word and makes a sentance out of it. 
+// or make a program that notifies me whenever the phillies are playing if i ca figure that out
+//Still want to do some more brainstorming
+## If you plan to collaborate with one or two classmates, what are their names?
+
+TODO // no 
+
+## Do you have any questions of your own?
+//no
+TODO
